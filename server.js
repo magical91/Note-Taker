785a46a13d42e4dcb0509b0ace58e21aff7c5ef3
magicalbeauty91 <magicalbeauty91@gmail.com>
2022-05-21 09:14:13 -0500

    linked the routes with the server

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const { notes } = require('./db/notes.json');
 const fs = require('fs');
+const { v4: uuidv4 } = require("uuid");
 const path = require('path');
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.get('/api/notes', (req, res) => {
-    res.json({ notes });
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
